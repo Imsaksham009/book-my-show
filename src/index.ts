4c1d36fc538358a98392e1107ba085594d97a606
Saksham <guptasaksham82@gmail.com>
2026-09-19 00:12:35 +0530

    scafold project

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import express, { type Express, type Request, type Response } from "express";
+import { pool } from "./db/pool";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+async function start() {
+	try {
+		await pool.connect();
+		await pool.query("SELECT 1"); // Test the database connection
+		console.log("Connected to the database");
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	} catch (error) {
+		console.error("Error connecting to the database:", error);
+		process.exit(1);
+	}
+}
+
+start();
